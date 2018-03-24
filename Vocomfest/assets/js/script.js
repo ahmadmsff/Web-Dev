2b7b77438d62530ac0b71b008dcc2a30b37eddcd
Achmad Musyaffa <ahmadmsff@gmail.com>
2018-03-24 17:44:01 +0700

    Update style

--- Vocomfest/assets/js/script.js
+++ Vocomfest/assets/js/script.js
@@ -20,4 +20,22 @@ $(document).ready(function() {
     });
     
 });
-        
+
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('#btnTop').fadeIn();
+		} else {
+			$('#btnTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('#btnTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
