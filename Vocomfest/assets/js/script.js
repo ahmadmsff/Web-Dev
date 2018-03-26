45164b9b1249b3a7c9da5e3fcb08c113e6ab672c
Achmad Musyaffa <ahmadmsff@gmail.com>
2018-03-26 07:23:31 +0700

    Adding slider

--- Vocomfest/assets/js/script.js
+++ Vocomfest/assets/js/script.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
     
-    var $item = $('div.item'), //Cache your DOM selector
+    // Slider
+    var slideIndex = 0;
+    showDivs(slideIndex);
+    
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");  
+        if (n < 1) {slideIndex = x.length}
+        for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+        slideIndex++;
+        if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";  
+        setTimeout(showDivs, 5000);
+    }
+    // End Slider
+
+    //  Horizontal Scroll
+    var $item = $('div.item'), 
         visible = 2, //Set the number of items that will be visible
         index = 0, //Starting index
         endIndex = ( $item.length / visible ) - 1; //End index
@@ -18,12 +37,9 @@ $(document).ready(function() {
           $item.animate({'left':'+=250px'});
         }
     });
+    // End Horizontal Scroll
     
-});
-
-$(document).ready(function(){
-	
-	//Check to see if the window is top if not then display button
+    //Scroll to top
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
 			$('#btnTop').fadeIn();
@@ -37,5 +53,5 @@ $(document).ready(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
-	
+    // End Scroll to top
 });
